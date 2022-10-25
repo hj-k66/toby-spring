72d76ecd03cb0183fdca2ad092b4708824ea9aa2
heejeong <hjx66@naver.com>
2022-10-25 14:26:03 +0900

    [refactor] JdbcTemplate 적용 - deleteAll(), add(), getCount(), findById()

--- src/main/java/com/toby/dao/UserDao.java
+++ src/main/java/com/toby/dao/UserDao.java
@@ -2,112 +2,42 @@ package com.toby.dao;
 
 import com.toby.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final DataSource dataSource;
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
     //생성자에서 초기화
     public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.jdbcContext = new JdbcContext(dataSource);
-
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
     public void deleteAll(){
-        this.jdbcContext.executeSql("DELETE FROM users");
+        this.jdbcTemplate.update("DELETE FROM users");
     }
 
     public int getCount(){
-        Connection c = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            // Query문 작성
-            pstmt = c.prepareStatement("SELECT count(*) FROM users;");
-
-            // Query문 실행
-            rs = pstmt.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if(rs != null){
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            if(pstmt != null){
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            if(c != null){
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-
+       return this.jdbcTemplate.queryForObject("SELECT count(*) from users", Integer.class);
     }
 
     public void add(User user) {
-        this.jdbcContext.workJdbcContextWithStatementStrategy(connection -> {
-            PreparedStatement pstmt = connection.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
-            pstmt.setString(1, user.getId());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
-            return pstmt;
-        });
+        this.jdbcTemplate.update("INSERT INTO users(id, name, password) values (?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
-
-    public User findById(String id) {
-
-        try {
-            Connection c = dataSource.getConnection();
-
-
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
-            pstmt.setString(1, id);
-
-            // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
-
-            User user = null;
-            if(rs.next()){
-                user = new User(rs.getString("id"), rs.getString("name"),
-                        rs.getString("password"));
-            }
-
-            rs.close();
-            pstmt.close();
-            c.close();
-
-            if(user==null){
-                throw new EmptyResultDataAccessException(1);
-            }
-
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
             return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
+    };
+    public User findById(String id) {
+        return this.jdbcTemplate.queryForObject("SELECT * from users WHERE id = ?", rowMapper,id);
     }
+
 
 }
