40c039fe90caff46bd007429e1c9f45ad270e2df
heejeong <hjx66@naver.com>
2022-10-25 11:43:04 +0900

    [refactor] Spring 적용

--- src/main/java/com/toby/dao/UserDaoFactory.java
+++ src/main/java/com/toby/dao/UserDaoFactory.java
@@ -1,11 +1,16 @@
 package com.toby.dao;
 
-public class UserDaoFactory {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class UserDaoFactory {
+    @Bean
     public UserDao awsUserDao() {
         UserDao userDao = new UserDao(new AwsConnectionMaker());
         return userDao;
     }
+    @Bean
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- src/test/java/com/toby/dao/UserDaoTest.java
+++ src/test/java/com/toby/dao/UserDaoTest.java
@@ -2,15 +2,26 @@ package com.toby.dao;
 
 import com.toby.domain.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.swing.*;
 
 import static org.assertj.core.api.Assertions.*;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
+
     @Test
     void addAndGet(){
         //Given
-        User user = new User("2","김희정","1234");
-        UserDao userDao = new UserDaoFactory().awsUserDao();
+        User user = new User("3","김희정","1234");
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
         //When
         userDao.add(user);
         User selectedUser = userDao.findById(user.getId());
